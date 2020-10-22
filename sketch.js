37e52499aacf72b66a1f45f758037cdcadfdfc0e
daksh100408 <69684809+daksh100408@users.noreply.github.com>
2020-10-22 18:30:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var sc,  mn, hr;
+
+ 
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  
+  sc = second();
+  mn = minute();
+  hr = hour();
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
